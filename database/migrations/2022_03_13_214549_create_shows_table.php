72a30732eb76073d0cd8a722a4aa1f8bed0ed5cc
Ishtehar Hussain <ishtehar@alley.co>
2022-03-13 20:38:38 -0400

    Migrations for `shows`, `venues`, `show_types`, and `show_slots`

---
+++ database/migrations/2022_03_13_214549_create_shows_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('db col default');
+            $table->foreignId('show_type_id')->nullable();
+            $table->json('meta')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('show_type_id')->references('id')->on('show_types')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shows');
+    }
+};
