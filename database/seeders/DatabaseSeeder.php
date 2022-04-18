94271e484fe3b401d8d3c6c3e7fa64ff88a8d757
Ishtehar Hussain <ishtehar@alley.co>
2022-04-17 21:39:43 -0400

    Update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Order;
 use App\Models\OrderContent;
+use App\Models\ShowType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use App\Models\Organizer;
 use App\Models\Show;
@@ -20,8 +22,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $types = ShowType::factory()
+            ->count(2)
+            ->create()
+            ->map(function ($item) {
+            return ['show_type_id' => $item->id];
+        })->values()->all();
+
         Organizer::factory(10)->has(
-            Show::factory()->has(
+            Show::factory()
+                ->state(new Sequence( ...$types ))
+                ->has(
                 ShowSlot::factory(2)->has(
                     Ticket::factory()
                         ->count(10)
@@ -31,7 +42,7 @@ class DatabaseSeeder extends Seeder
                                         ->state(['customer_id' => $ticket->belongs_to_id])])
                         )
                 ),
-            'showSlots'),
+            'slots'),
         'shows')
             ->create();
     }
